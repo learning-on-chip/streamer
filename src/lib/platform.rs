a3b05f91147dfb0552e8452fe7c1839918d7c33a
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-07-10 11:16:35 -0400

    Add a missing file

---
+++ src/lib/platform.rs
@@ -0,0 +1,51 @@
+use Result;
+use config::Config;
+use threed_ice::{Stack, StackElement};
+
+pub struct Platform {
+    pub elements: Vec<Element>,
+}
+
+pub struct Element {
+    pub name: String,
+}
+
+impl Platform {
+    pub fn new(config: &Config) -> Result<Platform> {
+        let path = path!(config, "a stack description");
+
+        info!(target: "platform", "Reading {:?}...", &path);
+        let stack = ok!(Stack::new(&path));
+
+        let mut elements = vec![];
+        for element in stack.elements {
+            match element {
+                StackElement::Die(ref die) => {
+                    for element in die.floorplan.elements.iter() {
+                        elements.push(Element {
+                            name: element.name.clone(),
+                        });
+                    }
+                },
+                _ => {},
+            }
+        }
+
+        Ok(Platform { elements: elements })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use config::Config;
+    use super::Platform;
+
+    #[test]
+    fn new() {
+        let config = Config::new("tests/fixtures/streamer.toml").unwrap()
+                            .branch("platform").unwrap();
+        let platform = Platform::new(&config).unwrap();
+        assert_eq!(platform.elements.iter().map(|element| &element.name).collect::<Vec<_>>(),
+                   &["Core0", "Core1", "Core2", "Core3"]);
+    }
+}
