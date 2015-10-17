68af1d4bf55204f81266bb9107b0f204cb5797ba
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-10-17 13:38:18 +0200

    Remove math.rs

--- src/lib/streamer.rs
+++ src/lib/streamer.rs
@@ -17,7 +17,6 @@ extern crate threed_ice;
 mod macros;
 
 mod event;
-mod math;
 mod platform;
 mod profile;
 mod result;
@@ -33,3 +32,16 @@ pub use system::{Increment, Job, System};
 
 pub type Config = configuration::Tree;
 pub type Source = random::Default;
+
+mod math {
+    #[link_name = "m"]
+    extern {
+        fn nextafter(x: f64, y: f64) -> f64;
+    }
+
+    #[inline]
+    pub fn next_after(x: f64) -> f64 {
+        use std::f64::INFINITY;
+        unsafe { nextafter(x, INFINITY) }
+    }
+}
